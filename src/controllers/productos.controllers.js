e08bba8f73fae83ed601205a30731351566bb262
Nicolas Powell <nicopowell30@gmail.com>
2023-07-07 18:53:02 -0300

    cree el modelo de usuario con mongoose

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,3 +1,4 @@
+import { validationResult } from "express-validator";
 import Producto from "../models/producto";
 
 export const controladorPrueba = (req, res) => {
@@ -6,6 +7,15 @@ export const controladorPrueba = (req, res) => {
 
 export const crearProducto = async (req, res) => {
     try {
+        //trabajar con los resultados de la validacion
+        const errors = validationResult(req);
+        //errors.isEmpty(), true:esta vacio, false:Tiene errores
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errores: errors.array(),
+            });
+        }
+
         const productoNuevo = new Producto(req.body);
         await productoNuevo.save();
         res.status(201).json({
@@ -50,8 +60,8 @@ export const borrarProducto = async (req, res) => {
         //Buscar en la BD la documento producto mediante el id
         await Producto.findByIdAndDelete(req.params.id);
         res.status(200).json({
-            mensaje: "EL producto fue eliminado correctamente"
-        })
+            mensaje: "EL producto fue eliminado correctamente",
+        });
     } catch (error) {
         console.log(error);
         res.status(404).json({
@@ -65,8 +75,8 @@ export const editarProducto = async (req, res) => {
         //Buscar en la BD la documento producto mediante el id
         await Producto.findByIdAndUpdate(req.params.id, req.body);
         res.status(200).json({
-            mensaje: "El producto fue actualizado correctamente"
-        })
+            mensaje: "El producto fue actualizado correctamente",
+        });
     } catch (error) {
         console.log(error);
         res.status(404).json({
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,10 +1,36 @@
 import { Router } from "express";
-import { borrarProducto, controladorPrueba, crearProducto, editarProducto, obtenerListaProductos, obtenerProducto } from "../controllers/productos.controllers";
+import {
+    borrarProducto,
+    controladorPrueba,
+    crearProducto,
+    editarProducto,
+    obtenerListaProductos,
+    obtenerProducto,
+} from "../controllers/productos.controllers";
+import { check } from "express-validator";
 
 const router = Router();
 
 router.route("/prueba").get(controladorPrueba);
-router.route("/productos").post(crearProducto).get(obtenerListaProductos);
+router
+    .route("/productos")
+    .post(
+        [
+            check("nombreProducto")
+                .notEmpty()
+                .withMessage("EL nombre del producto es obligatorio")
+                .isLength({ min: 2, max: 100 })
+                .withMessage("El nombre del producto debe contener entre 2 y 100 caracteres"),
+            check("precio")
+                .notEmpty()
+                .withMessage("El precio es obligatorio")
+                .isNumeric()
+                .withMessage("El precio debe ser un valor numerico")
+                .custom(()=>{}),
+        ],
+        crearProducto
+    )
+    .get(obtenerListaProductos);
 router.route("/productos/:id").get(obtenerProducto).delete(borrarProducto).put(editarProducto);
 
 export default router;
