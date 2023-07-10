c857ad2c55f7d4bef7949fef6cbf982063a97d2e
Nicolas Powell <nicopowell30@gmail.com>
2023-07-10 17:56:51 -0300

    agregamos el controlador del login

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,6 +1,6 @@
 import { validationResult } from "express-validator";
-import Usuario from "../models/usuario"
-import bcrypt from "bcrypt"
+import Usuario from "../models/usuario";
+import bcrypt from "bcrypt";
 export const obtenerListaUsuarios = async (req, res) => {
     try {
         //Buscar en la BD la collection de productos
@@ -31,11 +31,11 @@ export const crearUsuario = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        let usuario = await Usuario.findOne({ email })
-        if(usuario) {
+        let usuario = await Usuario.findOne({ email });
+        if (usuario) {
             return res.status(400).json({
-                mensaje: "Ya existe un usuario con el correo enviado"
-            })
+                mensaje: "Ya existe un usuario con el correo enviado",
+            });
         }
         const errors = validationResult(req);
 
@@ -49,8 +49,8 @@ export const crearUsuario = async (req, res) => {
         const usuarioNuevo = new Usuario(req.body);
 
         //encriptar el password
-        const salt = bcrypt.genSaltSync(10)
-        usuarioNuevo.password = bcrypt.hashSync(password, salt)
+        const salt = bcrypt.genSaltSync(10);
+        usuarioNuevo.password = bcrypt.hashSync(password, salt);
 
         await usuarioNuevo.save();
         res.status(201).json({
@@ -62,6 +62,40 @@ export const crearUsuario = async (req, res) => {
         console.log(error);
         res.status(404).json({
             mensaje: "Error al intentar crear un usuario",
+        });
+    }
+};
+
+export const login = async (req, res) => {
+    try {
+        //extraer el email y password del req.body
+        const { email, password } = req.body;
+        //verificamos que el mail existe en la BD
+        let usuario = await Usuario.findOne({ email });
+        if (!usuario) {
+            //Si no encuentro el usuario
+            return res.status(404).json({
+                mensaje: "Email o password incorrecto - correo",
+            });
+        }
+        //verificar si las contraseñas coinciden
+        const passwordValido = bcrypt.compareSync(password, usuario.password); //Devuelve un valor booleano
+        //Preguntar si la variable es invalida
+        if (!passwordValido) {
+            return res.status(404).json({
+                mensaje: "Email o password incorrecto - password",
+            });
+        }
+
+        //responder al frontend con el usuario valido
+        res.status(200).json({
+            mensaje: "El usuario es correcto",
+            nombreUsuario: usuario.nombreUsuario
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje: "Usuario o password incorrecto",
         });
     }
 };
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import {
     crearUsuario,
+    login,
     obtenerListaUsuarios,
     obtenerUsuario,
-} from "../controllers/usuarios.controllers";
+    } from "../controllers/usuarios.controllers";
 import { check } from "express-validator";
 
 const router = Router();
@@ -29,4 +30,19 @@ router
     );
 router.route("/usuarios/:id").get(obtenerUsuario);
 
+router.route("/usuario/login").post(
+  [
+      check("email", "El email es obligatorio").isEmail(),
+      check("password", "El password debe de ser de 6 caracteres")
+        .isLength({
+          min: 6,
+          max: 15,
+        })
+        .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)
+        .withMessage(
+          "El password debe contener 8 caracteres (al menos 1 letra mayúscula, 1 letra minúscula y 1 numero) también puede incluir carácteres especiales"
+        ),
+    ],
+    login
+);
 export default router;
